9ef293a511d3fc859a79ccd3d69b59c9426bfe5d
Sridatta Thatipamala <sthatipamala@gmail.com>
2013-04-05 18:50:02 -0700

    fix variety of compiler errors in router.c

--- router.c
+++ router.c
@@ -8,16 +8,20 @@
 #include <errno.h>
 #include <sys/socket.h>
 #include <netdb.h>
-#include <aw_queue.h>
+#include <fcntl.h>
+
+#include "router.h"
+#include "queue.h"
+#include "packet.h"
 
 const unsigned long MAX_PACKETS = 500;
 
-int main(int argc, char* arv[]) {
+int main(int argc, char* argv[]) {
   int streams = 1;
 
   /* Arguments: L, B, duty cycle, destination port 1, destination port 2 (optional) */
-  if (argc != 6 && argc != 8 && argc != 9) {
-    fprintf(stderr, "Usage: %s L B source_port_1 destination_port_1 [source_port_2 destination_port_2 duty_cycle [prioritized]]", argv[0]);
+  if (argc != 5 && argc != 8 && argc != 9) {
+    fprintf(stderr, "Usage: %s L B source_port_1 destination_port_1 [source_port_2 destination_port_2 duty_cycle [prioritized]]\n", argv[0]);
     return -1;
   }
   if (argc > 6) {
@@ -29,15 +33,15 @@ int main(int argc, char* arv[]) {
 
 	struct addrinfo* p_1;
   int insock_1 = recv_port(NULL, argv[3]);
-  int outsock_1 = send_port("localhost", argv[4], &p1);
+  int outsock_1 = send_port("localhost", argv[4], &p_1);
 
 	struct addrinfo* p_2 = NULL;
-  int insock2 = -1;
+  int insock_2 = -1;
   int outsock_2 = -1;
   int duty_cycle = 100;
   if (streams == 2) {
     insock_2 = recv_port(NULL, argv[5]);
-    outsock_2 = send_port("localhost", argv[6] &p_2);
+    outsock_2 = send_port("localhost", argv[6], &p_2);
     duty_cycle = strtol(argv[7], NULL, 10);
     if (duty_cycle == 0) {
       printf("Duty cycle zero or not numeric. Create a single-queue implementation by omitting later arguments.");
@@ -61,10 +65,10 @@ int main(int argc, char* arv[]) {
 	unsigned capacity_2 = B - capacity_1;
 	bytequeue queue_1, queue_2;
 	unsigned long sum_1, sum_2;
-	if (bytequeue_init(queue_1, sizeof(ee122_packet), capacity_1) < 0) {
+	if (bytequeue_init(&queue_1, sizeof(ee122_packet), capacity_1) < 0) {
 		perror("Memory error allocating queue 1");
 	}
-	if (streams == 2 && bytequeue_init(queue_2, sizeof(ee122_packet), capacity_2) < 0) {
+	if (streams == 2 && bytequeue_init(&queue_2, sizeof(ee122_packet), capacity_2) < 0) {
 		perror("Memory error allocating queue 2");
 	}
 	float avg_1, avg_2;
@@ -80,7 +84,7 @@ int main(int argc, char* arv[]) {
     ee122_packet p;
     struct sockaddr src_addr;
     int src_len = sizeof(src_addr);
-    read_count = recvfrom(insock_1, &p, sizeof(p), &src_addr, &src_len);
+    read_count = recvfrom(insock_1, &p, sizeof(p), 0, &src_addr, &src_len);
 		/* read_count == -1 and errno == EWOULDBLOCk or EAGAIN indicates nothing there */
 		if (read_count == -1 && errno != EWOULDBLOCK && errno != EAGAIN)
 			perror("Error reading from socket 1");
@@ -88,41 +92,41 @@ int main(int argc, char* arv[]) {
 			perror("Socket 1 closed");
 		else if (read_count > 0) {
 			/* push packet */
-			if (p->stream == 'A') {
-				bytequeue_push(queue_1, &p);
+			if (p.stream == 'A') {
+				bytequeue_push(&queue_1, &p);
 			}
 			if (streams == 2) {
-				if (p->stream == 'B') {
-					bytequeue_push(queue_2, &p);
+				if (p.stream == 'B') {
+					bytequeue_push(&queue_2, &p);
 				}
 			}
 			count++;
 		}
 		
 		gettimeofday(&curr_time, NULL);
-		diff_timeval(&diff_time, &curr_time, &last_time);
+		timeval_subtract(&diff_time, &curr_time, &last_time);
       
-		if (diff_time->tv_sec * 1000000 + diff_time_tv_usec > L * 1000) {
+		if (diff_time.tv_sec * 1000000 + diff_time.tv_usec > L * 1000) {
 			/* update average queue length */
 			sent_count++;
-			sum_1 += queue_1->filled;
+			sum_1 += queue_1.filled;
 			avg_1 = ((float)(sum_1)) / ((float)(sent_count));
 			if (streams == 2) {
-				sum_2 += queue_2->filled;
+				sum_2 += queue_2.filled;
 				avg_2 = ((float)(sum_2)) / ((float)(sent_count));
 			}
 
 			ee122_packet s;
 			int write_count = 0;
 			if (prioritized) {
-				if (queue_1->filled) {
-					if (bytequeue_pop(queue_1, &s) == 0) {
+				if (queue_1.filled) {
+					if (bytequeue_pop(&queue_1, &s) == 0) {
 						s.avg_len = avg_1;
 						sendto(outsock_1, &s, sizeof(s), 0, p_1->ai_addr, p_1->ai_addrlen);
 					}
 				}
 				else {
-					if (bytequeue_pop(queue_2, &s) == 0) {
+					if (bytequeue_pop(&queue_2, &s) == 0) {
 						s.avg_len = avg_2;
 						sendto(outsock_2, &s, sizeof(s), 0, p_2->ai_addr, p_2->ai_addrlen);
 					}
@@ -131,14 +135,14 @@ int main(int argc, char* arv[]) {
 			else {
 				if (streams == 2) {
   				if (which_queue == 1) {
-  					if (bytequeue_pop(queue_1, &s) == 0) {
+  					if (bytequeue_pop(&queue_1, &s) == 0) {
 						  s.avg_len = avg_1;
 							sendto(outsock_1, &s, sizeof(s), 0, p_1->ai_addr, p_1->ai_addrlen);
   					}
   					which_queue = 2;
   				}
   				else {
-  					if (bytequeue_pop(queue_2, &s) == 0) {
+  					if (bytequeue_pop(&queue_2, &s) == 0) {
 							s.avg_len = avg_2;
 							sendto(outsock_2, &s, sizeof(s), 0, p_2->ai_addr, p_2->ai_addrlen);
   					}
@@ -158,7 +162,7 @@ int main(int argc, char* arv[]) {
 	}
 }
 
-int recv_port(char[] host, char[] port) {
+int recv_port(char* host, char* port) {
   int status;
   int sockfd;
 
@@ -201,7 +205,7 @@ int recv_port(char[] host, char[] port) {
   return sockfd;
 }
 
-int send_port(char[] host, char[] port, struct addrinfo** pptr) {
+int send_port(char* host, char* port, struct addrinfo** pptr) {
   int status;
   int sockfd;
 
