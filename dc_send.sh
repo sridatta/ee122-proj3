28a6ba0e5651adebbfa6059607d99fde401708b7
Andrew Wygle <awygle@gmail.com>
2013-04-07 19:11:55 -0700

    Sender adn receiver scripts

--- dc_send.sh
+++ dc_send.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "Gathering data on receiver machine."
+echo "Gathering data on sender machine."
 
 ITERATIONS=3
 
@@ -9,8 +9,8 @@ for i in {1..$ITERATIONS} do
     echo "Iteration $i"
     for R in {5..20..5} do
         echo "R = $R"
-        ./router 10 64 5432 5433 2>&1 > /dev/null &
-        ./receiver 5433 > task_1_iter_$i_R_$R.csv
+        ./sender $1 5432 $R A
+        sleep 2
     done
 done
 
@@ -19,9 +19,9 @@ for i in {1..$ITERATIONS} do
     echo "Iteration $i"
     for R in {5..20..5} do
         echo "R = $R"
-        ./router 10 64 5432 5433 5434 5435 50 2>&1 > /dev/null &
-        ./receiver 5433 > task2_iter_$i_R_$R_recv_1.csv &
-        ./receiver 5435 > task2_iter_$i_R_$R_recv_2.csv
+        ./sender $1 5432 $R A &
+        ./sender $1 5434 $R B
+        sleep 2
     done
 done
 
@@ -30,9 +30,9 @@ for i in {1..$ITERATIONS} do
     echo "Iteration $i"
     for R in {5..20..5} do
         echo "R = $R"
-        ./router 10 64 5432 5433 5434 5435 50 yes 2>&1 > /dev/null &
-        ./receiver 5433 > task31_iter_$i_R_$R_recv_1.csv &
-        ./receiver 5435 > task31_iter_$i_R_$R_recv_2.csv
+        ./sender $1 5432 $R A &
+        ./sender $1 5434 $R B
+        sleep 2
     done
 done
 
@@ -42,21 +42,22 @@ for i in {1..$ITERATIONS} do
     for R in {5..20..5} do
         echo "R = $R"
         for B in {32..128..32} do
-            ./router 10 $B 5432 5433 5434 5435 50 yes 2>&1 > /dev/null &
-            ./receiver 5433 > task32_iter_$i_R_$R_B_$B_recv_1.csv &
-            ./receiver 5435 > task32_iter_$i_R_$R_B_$B_recv_2.csv
+            ./sender $1 5432 $R A &
+            ./sender $1 5434 $R B
+            sleep 2
          done
     done
 done
 
+R = 12
 echo "Task 3.3: " 
 for i in {1..$ITERATIONS} do
     echo "Iteration $i"
     for L in {5..20..5} do
-        echo "L = $L"
-        ./router $L 64 5432 5433 5434 5435 50 yes 2>&1 > /dev/null &
-        ./receiver 5433 > task33_iter_$i_B_$B_recv_1.csv &
-        ./receiver 5435 > task33_iter_$i_B_$B_recv_2.csv
+        echo "R = 12"
+         ./sender $1 5432 $R A &
+         ./sender $1 5434 $R B
+         sleep 2
     done
 done
 
@@ -64,12 +65,12 @@ echo "Task 3.4: "
 for i in {1..$ITERATIONS} do
     echo "Iteration $i"
     for L in {5..20..5} do
-        echo "L = $L"
+        echo "R = $R"
         for B in {32..128..32} do
-            ./router $L $B 5432 5433 5434 5435 50 yes 2>&1 > /dev/null &
-            ./receiver 5433 > task34_iter_$i_L_$L_B_$B_recv_1.csv &
-            ./receiver 5435 > task34_iter_$i_L_$L_B_$B_recv_2.csv
-         done
+         ./sender $1 5432 $R A &
+         ./sender $1 5434 $R B
+         sleep 2
+        done
     done
 done
 
