225929f519cc49f1d2d024201e463bc3e7c5e7a9
Sridatta Thatipamala <sthatipamala@gmail.com>
2013-04-07 21:48:55 -0700

    output results to ./results dir

--- dc_recv.sh
+++ dc_recv.sh
@@ -12,7 +12,7 @@ do
     do
         echo "R = $R"
         ./router 10 64 5432 5433 2>&1 > /dev/null &
-        ./receiver 5433 > task_1_iter_${i}_R_${R}.csv
+        ./receiver 5433 >> ./results/task_1.csv
     done
 done
 
@@ -24,8 +24,8 @@ do
     do
         echo "R = $R"
         ./router 10 64 5432 5433 5434 5435 50 2>&1 > /dev/null &
-        ./receiver 5433 > task2_iter_${i}_R_${R}_recv_1.csv &
-        ./receiver 5435 > task2_iter_${i}_R_${R}_recv_2.csv
+        ./receiver 5433 >> ./results/task2_recv_1.csv &
+        ./receiver 5435 >> ./results/task2_recv_2.csv
     done
 done
 
@@ -37,8 +37,8 @@ do
     do
         echo "R = $R"
         ./router 10 64 5432 5433 5434 5435 50 yes 2>&1 > /dev/null &
-        ./receiver 5433 > task31_iter_${i}_R_${R}_recv_1.csv &
-        ./receiver 5435 > task31_iter_${i}_R_${R}_recv_2.csv
+        ./receiver 5433 >> ./results/task31_recv_1.csv &
+        ./receiver 5435 >> ./results/task31_recv_2.csv
     done
 done
 
@@ -52,8 +52,8 @@ do
         for B in $(seq 32 32 128) 
         do
             ./router 10 $B 5432 5433 5434 5435 50 yes 2>&1 > /dev/null &
-            ./receiver 5433 > task32_iter_${i}_R_${R}_B_${B}_recv_1.csv &
-            ./receiver 5435 > task32_iter_${i}_R_${R}_B_${B}_recv_2.csv
+            ./receiver 5433 >> ./results/task32_B_${B}_recv_1.csv &
+            ./receiver 5435 >> ./results/task32_B_${B}_recv_2.csv
          done
     done
 done
@@ -66,8 +66,8 @@ do
     do
         echo "L = $L"
         ./router $L 64 5432 5433 5434 5435 50 yes 2>&1 > /dev/null &
-        ./receiver 5433 > task33_iter_${i}_B_${B}_recv_1.csv &
-        ./receiver 5435 > task33_iter_${i}_B_${B}_recv_2.csv
+        ./receiver 5433 >> ./results/task33_B_${B}_recv_1.csv &
+        ./receiver 5435 >> ./results/task33_B_${B}_recv_2.csv
     done
 done
 
@@ -81,8 +81,8 @@ do
         for B in $(seq 32 32 128) 
         do
             ./router $L $B 5432 5433 5434 5435 50 yes 2>&1 > /dev/null &
-            ./receiver 5433 > task34_iter_${i}_L_${L}_B_${B}_recv_1.csv &
-            ./receiver 5435 > task34_iter_${i}_L_${L}_B_${B}_recv_2.csv
+            ./receiver 5433 >> ./results/task34_L_${L}_B_${B}_recv_1.csv &
+            ./receiver 5435 >> ./results/task34_L_${L}_B_${B}_recv_2.csv
          done
     done
 done
