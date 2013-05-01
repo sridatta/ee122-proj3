ce07b7a4531d62009ec6b468c27739798357c31d
Sridatta Thatipamala <sthatipamala@gmail.com>
2013-04-30 21:48:01 -0700

    some kind of implementation of ARQ

--- sender.h
+++ sender.h
@@ -1,2 +1,5 @@
-long rand_poisson(long lambda);
+float rand_poisson(long lambda);
 int main(int argc, char *argv[]);
+int recv_port(char* host, char* port);
+int send_port(char* host, char* port, struct addrinfo** pptr);
+int timeval_subtract(struct timeval *result, struct timeval *x, struct timeval *y);
